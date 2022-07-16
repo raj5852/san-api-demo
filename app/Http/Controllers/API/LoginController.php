38a144bead26c2dd8b1e4b336615c6b916190e42
Raj <rajkumer2001@gmail.com>
2022-07-16 17:47:02 +0600

    santum

---
+++ app/Http/Controllers/API/LoginController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    function  login(Request $request)
+    {
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = Auth::user();
+            $success['token'] =  $user->createToken('MyApp')->plainTextToken;
+            $success['name'] =  $user->name;
+
+            return response()->json($success);
+        } else {
+            return response()->json('Unauthorised');
+        }
+    }
+}
